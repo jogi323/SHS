b5f5f205f9600fc97f44ede49b56990e8f66e9ec
Joginaidu Gopisetti <jgopisetti@corp.innominds.com>
2017-07-06 11:33:58 +0530

    done

--- app.js
+++ app.js
@@ -20,11 +20,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/dist/index.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+app.get('/*', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html'); // uncommented, since it didn't work
+});
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   var err = new Error('Not Found');
